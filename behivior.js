b141e47265754bd1213e001dfc0c247b1b5d5402
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-07-16 18:32:30 -0400

    Update README.md

--- behivior.js
+++ behivior.js
@@ -3,10 +3,19 @@ import { MountObserver } from 'mount-observer/MountObserver.js';
 export const emc = {
     enhPropKey: 'beAlit',
     base: 'be-alit',
+    branches: ['', 'vm', 'eval'],
     map: {
         '0.0': {
             instanceOf: 'Object$entences',
             objValMapsTo: '.',
+        },
+        '1.0': {
+            instanceOf: 'Object',
+            mapsTo: 'vm'
+        },
+        '2.0': {
+            instanceOf: 'String',
+            mapsTo: 'eval'
         }
     },
     importEnh: async () => {
--- behivior.ts
+++ behivior.ts
@@ -5,10 +5,19 @@ import {AP} from './types';
 export const emc: EMC<any, AP> = {
     enhPropKey: 'beAlit',
     base: 'be-alit',
+    branches: ['', 'vm', 'eval'],
     map: {
         '0.0': {
             instanceOf: 'Object$entences',
             objValMapsTo: '.',
+        },
+        '1.0':{
+            instanceOf: 'Object',
+            mapsTo: 'vm'
+        },
+        '2.0': {
+            instanceOf: 'String',
+            mapsTo: 'eval'
         }
     },
     importEnh: async () => {
