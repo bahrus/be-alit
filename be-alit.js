bc2f000d88b0ce0f4053dcbf0877323de2558bb4
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-05 17:09:31 -0500

    Merge branch 'baseline' of https://github.com/bahrus/be-alit into baseline

--- be-alit.js
+++ be-alit.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { BE } from 'be-enhanced/BE.js';
 import { propInfo } from 'be-enhanced/cc.js';
-import {render} from 'lit-html';
+import {render, html} from 'lit-html';
 
 /** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types.d.ts' */
 /** @import {Actions, PAP,  AP, BAP} from './ts-refs/be-alit/types' */;
@@ -23,6 +23,9 @@ class BeAlit extends BE {
             renderer: {},
         },
         actions: {
+            getRenderer: {
+                ifNoneOf: ['renderer']
+            },
             doRender: {
                 ifAllOf: ['renderer', 'vm']
             }
@@ -32,11 +35,27 @@ class BeAlit extends BE {
     /**
      * 
      * @param {BAP} self 
+     * @returns 
+     */
+    getRenderer(self){
+        const {enhancedElement} = self;
+        return /** @type {BAP} */({
+            renderer: enhancedElement.renderer
+        });
+    }
+
+    /**
+     * 
+     * @param {BAP} self 
      */
     doRender(self) {
         const {renderer, vm, enhancedElement} = self;
-        render(renderer(vm), enhancedElement);
+        const {parentElement} = enhancedElement;
+        if(parentElement === null) throw 400;
+        render(renderer(vm, html), parentElement);
     }
+
+    
 }
 
 await BeAlit.bootUp();
