65d10473c759cd1fe362afa07b275c751d5e4162
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-06 15:32:44 -0500

    Merge branch 'baseline' of https://github.com/bahrus/be-alit into baseline

--- emc.js
+++ emc.js
@@ -1,26 +1,16 @@
 // @ts-check
 import { BeHive, seed, MountObserver } from 'be-hive/be-hive.js';
+import { emc as baseEMC } from 'be-render-neutral/emc.js';
 /** @import {EMC, EventListenerOrFn} from './ts-refs/trans-render/be/types' */
-/** @import {Actions, PAP,  AP} from './ts-refs/be-alit/types' */;
+/** @import {Actions, PAP,  AP} from './ts-refs/be-render-neutral/types' */;
 /** @import {CSSQuery} from './ts-refs/trans-render/types.js' */
 
 /**
  * @type {Partial<EMC<any, AP>>}
  */
 export const emc = {
+    ...baseEMC,
     base: 'be-alit',
-    branches: ['', 'vm', 'with'],
-    map: {
-        '1.0': {
-            instanceOf: 'Object',
-            mapsTo: 'vm'
-        },
-        '2.0': {
-            instanceOf: 'DSSArray',
-            arrValMapsTo: 'with'
-        }
-    },
-    enhancedElementInstanceOf: [HTMLScriptElement],
     enhPropKey: 'beAlit',
     importEnh: async () => {
         const { BeAlit } = 
@@ -29,8 +19,6 @@ export const emc = {
         (await import('./be-alit.js'));
         return BeAlit;
     },
-    ws: [],
-    primaryProp: 'renderer'
 };
 
 const mose = seed(emc);
--- 🎇.js
+++ 🎇.js
@@ -3,7 +3,7 @@ import { MountObserver, seed, BeHive } from 'be-hive/be-hive.js';
 import { emc as baseEMC } from './emc.js';
 /** @import  {EMC, EventListenerOrFn} from './ts-refs/trans-render/be/types' */;
 /** @import {CSSQuery} from './ts-refs/trans-render/types.js' */
-/** @import {Actions, PAP,  AP} from './ts-refs/be-calculating/types' */;
+/** @import {Actions, PAP,  AP} from './ts-refs/be-render-neutral/types' */;
 
 /**
  * @type {Partial<EMC<any, AP>>}
@@ -13,7 +13,6 @@ export const emc = {
     base: '🎇',
     enhPropKey: '🎇',
     handlerKey: '🎇',
-    ws: []
 };
 const mose = seed(emc);
 MountObserver.synthesize(document, BeHive, mose);
