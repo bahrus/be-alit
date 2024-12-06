b54516bc2e59a70ac7efc85e05dd767c9628b871
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-06 14:54:28 -0500

    Merge branch 'baseline' of https://github.com/bahrus/be-alit into baseline

--- 🎇.js
+++ 🎇.js
@@ -1,9 +1,6 @@
 // @ts-check
 import { MountObserver, seed, BeHive } from 'be-hive/be-hive.js';
 import { emc as baseEMC } from './emc.js';
-import {Registry} from 'be-hive/Registry.js';
-import {aggs} from 'be-hive/aggEvt.js';
-import { w as bw } from 'be-hive/w.js';
 /** @import  {EMC, EventListenerOrFn} from './ts-refs/trans-render/be/types' */;
 /** @import {CSSQuery} from './ts-refs/trans-render/types.js' */
 /** @import {Actions, PAP,  AP} from './ts-refs/be-calculating/types' */;
@@ -21,13 +18,6 @@ export const emc = {
 const mose = seed(emc);
 MountObserver.synthesize(document, BeHive, mose);
 
-/**
- * 
- * @param {CSSQuery} q 
- */
-export function w(q){
-    return bw(q, emc.ws, w);
-}
 
 
 
