7b9c2a17a178b1b8b146fe998fbcaad715681213
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-09-20 08:49:54 -0400

    Merge branch 'baseline' of https://github.com/bahrus/be-alit into baseline

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@
 import { PlaywrightTestConfig, devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: 'python ./node_modules/ssi-server/ssi_server.py',
+    command: 'node ./node_modules/spa-ssi/serve.js',
     url: 'http://localhost:8000/',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
